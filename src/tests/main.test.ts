956d0d54e45aeca85602860e1101103c2b07ad84
Drairan9 <drairan@protonmail.com>
2023-10-17 00:44:55 +0200

    enviroment ready for unit tests

---
+++ src/tests/main.test.ts
@@ -0,0 +1,10 @@
+import { SpotifyTrack } from 'play-dl';
+import SearchService from '../services/search_service';
+
+function x(num: number) {
+    return num + num;
+}
+
+test('testing', () => {
+    expect(x(1)).toBe(2);
+});
