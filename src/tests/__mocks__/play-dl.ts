2e4d6fe4bca262d45b6887d7386215a8a598bcc5
Drairan9 <drairan@protonmail.com>
2023-10-20 17:51:02 +0200

    New tests mocking strucutre + more requests mocked

--- src/tests/__mocks__/play-dl.ts
+++ src/tests/__mocks__/play-dl.ts
@@ -1,15 +1,72 @@
-import play from 'play-dl';
-import { YouTubeVideo as DummyYoutubeVideo } from './responses/YoutubeVideo';
+import play, { Spotify, SpotifyAlbum, SpotifyPlaylist, SpotifyTrack, YouTubeVideo } from 'play-dl';
+import { musicEnum } from '../../utils/enums';
 
 const search = async (x: any, y: any): Promise<any> => {
     return new Promise((resolve, reject) => {
-        resolve([new DummyYoutubeVideo({})]);
+        resolve([
+            new YouTubeVideo({
+                id: 'xxx',
+                url: `https://www.youtube.com/watch?v=xxx`,
+                title: 'xxx',
+                description: 'x',
+                duration: 0,
+                duration_raw: '0:00',
+                thumbnails: [
+                    {
+                        url: 'xxx',
+                        width: 2,
+                        height: 2,
+                    },
+                    {
+                        url: 'xxx',
+                        width: 2,
+                        height: 2,
+                    },
+                ],
+                channel: {
+                    id: 'xxx',
+                    name: 'xxx',
+                    url: `xxx`,
+                    icons: [],
+                    verified: true,
+                    artist: true,
+                },
+                uploadedAt: '2023-03-20T07:00:01-07:00',
+                upcoming: undefined,
+                views: 0,
+                live: false,
+            }),
+        ]);
     });
 };
 
+const so_validate = async (url: string): Promise<false | musicEnum> => {
+    return new Promise((resolve, reject) => {
+        switch (url) {
+            case 'https://soundcloud.com/hexibase/oho':
+                resolve(musicEnum.Track);
+            case 'https://soundcloud.com/user-802956580/sets/hexabase':
+                resolve(musicEnum.Playlist);
+            case 'https://soundcloud.com/hexibase':
+                resolve(musicEnum.Search);
+            case 'Test query':
+                resolve(musicEnum.Search);
+            default:
+                resolve(false);
+        }
+    });
+};
+
+const is_expired = () => {
+    return false;
+};
+
 play.search = search;
+play.is_expired = is_expired;
 
 module.exports = {
     ...jest.requireActual('play-dl'),
     search,
+    is_expired,
+    so_validate,
 };
---
+++ src/tests/query_provider.test.ts
@@ -0,0 +1,9 @@
+import SearchService from '../services/search_service';
+
+const _SearchService = new SearchService();
+
+// describe('Querying providers', () => {
+//     test('Query Spotify', async () => {
+//         expect(await _SearchService.searchYoutubeVideo('xxx')).toBe('x');
+//     });
+// });
